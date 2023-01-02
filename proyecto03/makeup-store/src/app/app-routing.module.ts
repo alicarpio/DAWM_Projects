1c3e9cd1690566f8d33b62a06b98a1776c87fcb2
alicarpio <agcarpio@espol.edu.ec>
2023-01-01 21:34:36 -0500

    add routes and principal components

--- proyecto03/makeup-store/src/app/app-routing.module.ts
+++ proyecto03/makeup-store/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {ShopComponent} from "./shop/shop.component";
+import {ProductInfoComponent} from "./product-info/product-info.component";
+import {CartComponent} from "./cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "shop", component: ShopComponent },
+  { path: "productInfo", component: ProductInfoComponent },
+  { path: "cart", component: CartComponent },
+  { path: "**", redirectTo: "ShopComponent" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyecto03/makeup-store/src/app/app.module.ts
+++ proyecto03/makeup-store/src/app/app.module.ts
@@ -1,19 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { CartComponent } from './cart/cart.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ShopComponent,
+    ProductInfoComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserAnimationsModule
   ],
   providers: [],
