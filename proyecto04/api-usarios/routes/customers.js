ce305a9917c43455680cb74949858d41e4882b11
alicarpio <agcarpio@espol.edu.ec>
2023-01-22 13:13:15 -0500

    add api usuarios

---
+++ proyecto04/api-usarios/routes/customers.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+const Sequelize = require('sequelize');
+const Customer = require('../models').customer;
+
+router.get('/findAll/json', function(req, res, next) {
+    Customer.findAll({
+        attributes: {exclude: ["updatedAt", "createdAt","id"]}
+    }).then(customers => {
+        res.json(customers);
+    }).catch(error => res.status(400).send(error))
+});
+
+
+module.exports = router;
